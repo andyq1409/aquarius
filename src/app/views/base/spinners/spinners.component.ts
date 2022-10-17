2e1a4a9507992ab9a5a5e8a9b4bca45519a9b400
andyq1409 <andyq1409@gmail.com>
2022-10-17 11:55:19 +0200

    first commit

---
+++ src/app/views/base/spinners/spinners.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinners',
+  templateUrl: './spinners.component.html',
+  styleUrls: ['./spinners.component.css']
+})
+export class SpinnersComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/views/forms/floating-labels/floating-labels.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-labels',
+  templateUrl: './floating-labels.component.html',
+  styleUrls: ['./floating-labels.component.css']
+})
+export class FloatingLabelsComponent {
+
+  constructor() { }
+
+}
